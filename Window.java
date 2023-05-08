18c50274240b0b03f162630aa9bc7ed373f3e387
DerekKirschbaum <dgkirschbaum@gmail.com>
2023-05-08 00:17:14 -0700

    added speed control slider

--- Window.java
+++ Window.java
@@ -2,6 +2,9 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.Timer;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 import java.util.ArrayList;
 
 
@@ -11,18 +14,31 @@ public class Window extends JFrame{
     private boolean startPressed = false;
     private int x = 200;
     private int y = 200;
-    private int z = 0;
+    private int numIterations = 0;
     private ArrayList<Point> points = new ArrayList<>();
+    private int speed = 1;
+    private Timer t;
 
     public Window(){
         JPanel bp = new JPanel(new FlowLayout());
         JButton start = new JButton("Start");
-        System.out.println("Here");
+        JSlider js = new JSlider(1, 23, 1);
+        JLabel l = new JLabel("     Speed: " + js.getValue());
+        JLabel l2 = new JLabel("     Points: " + points.size());
         bp.add(start);
+        bp.add(l);
+        bp.add(js);
+        bp.add(l2);
+        
+        js.addChangeListener(new ChangeListener () {
+            public void stateChanged(ChangeEvent e) {
+                l.setText("     Speed : " + ((JSlider)e.getSource()).getValue());
+                speed = ((JSlider)e.getSource()).getValue();
+            }
+        });
 
         start.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
-                System.out.println("btn");
                 startPressed = true;
                 p.repaint();
                 //requestFocus(); // change the focus to JFrame to receive KeyEvent
@@ -47,18 +63,18 @@ public class Window extends JFrame{
             @Override
             public void actionPerformed(ActionEvent evt) {
                 
-                if (z < 10000 && startPressed) {
+                if (numIterations < 10000 && startPressed) {
                     update();
                     repaint();
-                    z++;
-                    
+                    numIterations++;
+                    l2.setText("     Points: " + points.size());
                 }
             
             }
         };
-        System.out.println("Here");
         
-        new Timer(0, updateTask).start();// Allocate a Timer to run updateTask's actionPerformed() after every delay msec
+        t = new Timer(((int)(1000/(Math.pow((double)speed, 2.0)))), updateTask);
+        t.start();// Allocate a Timer to run updateTask's actionPerformed() after every delay msec
         
     }
 
@@ -119,11 +135,16 @@ public class Window extends JFrame{
             for(Point p: points){
                 g2d.drawLine(p.x, p.y, p.x, p.y);
             }
-            if(startPressed && z<9999){
+            if(startPressed && numIterations<9999 && points.size()>1){
                 g2d.drawOval(points.get(points.size()-1).x, points.get(points.size()-1).y, 3, 3);
                 g2d.setColor(Color.RED);
                 g2d.fillOval(points.get(points.size()-1).x, points.get(points.size()-1).y, 3, 3);
             }
+            
+            int a = (int)(1000/(Math.pow((double)speed, 2.0)));
+            t.setDelay(a);
+            System.out.println(a);
+
             
         }
     }
